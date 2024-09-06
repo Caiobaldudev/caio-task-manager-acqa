2ada2558b5e931acb4adbb64d33b4c829bd1475c
Caio Balduino <caiobalduinodev@hotmail.com>
2024-09-06 16:54:44 -0300

    feat: apply context

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import "./global.css";
 import AddTask from './components/AddTask/AddTask';
 import ToDoList from './components/ToDoList/ToDoList';
+import { ToDoProvider } from './contexts/ToDoContext'; 
 
 const App = () => {
   return (
-    <div className='container'>
+    <ToDoProvider>
+    <div className="container">
       <h1>Gerencie suas tarefas</h1>
-      <AddTask/>
-      <ToDoList/>
+      <AddTask />
+      <ToDoList />
     </div>
+  </ToDoProvider>
   )
 }
 
