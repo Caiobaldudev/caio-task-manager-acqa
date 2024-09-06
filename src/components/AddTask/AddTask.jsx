ae6757e410427db25ad7952fe2e525243cf23254
Caio Balduino <caiobalduinodev@hotmail.com>
2024-09-06 15:15:10 -0300

    feat: Add task component & style

--- src/components/AddTask/AddTask.jsx
+++ src/components/AddTask/AddTask.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
+import Input from "../Input/Input";
+import { useToDo } from '../../contexts/ToDoContext.jsx';
+import styles from"./AddTask.module.css";
 
 const AddTask = () => {
-  return (
-    <div>
-      Add a Task
-    </div>
-  )
-}
+  const [task, setTask] = React.useState("");
+  const { dispatch } = useToDo();
 
-export default AddTask
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (task.trim()) {
+      dispatch({ type: 'ADD_TASK', payload: task });  
+      setTask(""); 
+    }
+  };
+
+  return (
+    <div className={styles.taskContainer}>
+      <form onSubmit={handleSubmit}>
+        <Input
+          id="task"
+          type="text"
+          label="Tarefa"
+          value={task}
+          setValue={setTask}
+          onChange={(e) => setTask(e.target.value)}
+          placeholder="Digite sua nova tarefa!"
+          required
+        />
+        <button type="submit">Adicionar Tarefa</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddTask;
