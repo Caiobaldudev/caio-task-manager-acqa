f177c565a2cff3ab4adffa4946a585986ddc1902
Caio Balduino <caiobalduinodev@hotmail.com>
2024-09-06 11:51:41 -0300

    feat: initial structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import "./global.css";
+import AddTask from './components/AddTask/AddTask';
+import ToDoList from './components/ToDoList/ToDoList';
 
 const App = () => {
   return (
-    <div>
-      Initial
+    <div className='container'>
+      <h1>Gerencie suas tarefas</h1>
+      <AddTask/>
+      <ToDoList/>
     </div>
   )
 }
--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,5 @@ import App from './App.jsx'
 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <App />
-  </StrictMode>,
 )
